2ce80691c8aeb114519ec6add1b73edee87c9af2
VictorBortoli <victorferreiradebortoli@gmail.com>
2021-05-06 16:01:59 -0300

    lacoderepeticaowhile

---
+++ src/br/com/senai/LacoDeRepeticaoWhile.java
@@ -0,0 +1,22 @@
+package br.com.senai;
+
+public class LacoDeRepeticaoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int valor = 0;
+		
+		System.out.println("Valor:" + valor);
+		
+		while(valor <= 25) {
+			System.out.println("-> valor:" + valor);
+			valor ++
+			valor += 1
+			valor = valor + 1;
+			
+		}
+		
+		System.out.println("Agora valor é:" + valor);
+	}
+
+}
